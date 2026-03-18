d4813c5c94e64268d36e73009c720b9e655e902d
Monica Toledo <monica.toledo@monicato-ltmfw72.internal.salesforce.com>
2026-03-18 18:55:34 -0500

    agentforce brief

--- templates/agentforceLeadBriefLwc/agentforceLeadBriefLwc.js
+++ templates/agentforceLeadBriefLwc/agentforceLeadBriefLwc.js
@@ -1,24 +1,32 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class AgentforceLeadBriefLwc extends LightningElement {
-    // Header
-    @api astroIconUrl = '{{{astroIconUrl}}}';
-    @api title = '{{{title}}}';
-    @api timestampText = '{{{timestampText}}}';
+  // Header
+  @api astroIconUrl = "{{{astroIconUrl}}}";
+  @api title = "{{{title}}}";
+  @api timestampText = "{{{timestampText}}}";
 
-    // Intent Section
-    @api intentHeading = '{{{intentHeading}}}';
-    @api intentText = '{{{intentText}}}';
+  // Intent Section
+  @api intentHeading = "{{{intentHeading}}}";
+  @api intentText = "{{{intentText}}}";
 
-    // Context Section
-    @api contextHeading = '{{{contextHeading}}}';
-    @api contextText = '{{{contextText}}}';
+  // Context Section
+  @api contextHeading = "{{{contextHeading}}}";
+  @api contextText = "{{{contextText}}}";
 
-    // Suggested Opener Section
-    @api openerHeading = '{{{openerHeading}}}';
-    @api openerText = '{{{openerText}}}';
+  // Suggested Opener Section
+  @api openerHeading = "{{{openerHeading}}}";
+  @api openerText = "{{{openerText}}}";
 
-    // Footer Buttons
-    @api primaryButtonText = '{{{primaryButtonText}}}';
-    @api secondaryButtonText = '{{{secondaryButtonText}}}';
+  // Footer Buttons
+  @api primaryButtonText = "{{{primaryButtonText}}}";
+  @api secondaryButtonText = "{{{secondaryButtonText}}}";
+
+  // Colors
+  @api bgColor = "{{{bgColor}}}";
+  @api textColor = "{{{textColor}}}";
+
+  get containerStyle() {
+    return `background-color: ${this.bgColor}; color: ${this.textColor};`;
+  }
 }
